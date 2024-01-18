df4d90b223ef68545b09a1f97fc358bcd7a676f0
AnmolSR98 <anmolratolcoding@gmail.com>
2024-01-17 21:26:07 -0700

    commit #2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import  Papa  from 'papaparse';
+import quotes from './quotes.csv';
+
+const images = require.context('./backgrounds', true);
+const imageList = images.keys().map(image => images(image));
+
+function getURL(string){
+
+  return "url("+string+")";
+
+}
+
+function getImage(){
+
+  return imageList[Math.floor(Math.random()*imageList.length)];
+
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+      const [quote, setQuote] = useState();
+
+      const useablequotes = () => {Papa.parse(quotes, {
+          download: true,
+          complete: function(results) {
+            setQuote(results.data[Math.floor(Math.random()*results.data.length)]);
+          }
+        });
+      }
+
+      const [backgroundURL, setBackgroundURL] = useState(getURL(getImage())); // getURL(picture1)
+
+      /**
+      const getThing = () => {
+        fetch('https://jsonplaceholder.typicode.com/photos')
+          .then(response => {
+            return response.json();
+          })
+          .then((data) => {
+            console.log(data);
+      })};**/
+
+      
+      useEffect(() => {
+
+        useablequotes();
+        document.body.style.backgroundImage = backgroundURL;
+        document.body.style.backgroundSize = "cover";
+        document.body.style.backgroundRepeat = "no-repeat";
+        document.body.style.backgroundAttachment = "fixed"; 
+        document.body.style.backgroundPosition = "center";
+      
+
+      }, [backgroundURL]);
+
+
+      return (<div>
+
+          <div class = 'content'><div class = "text">
+            {quote}
+          </div></div>
+
+      </div>);
+
 }
 
 export default App;
